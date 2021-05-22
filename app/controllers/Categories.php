3f410f826a00799e9cd8275cdbf8578816953078
admin <admin@admins-MacBook-Pro.local>
2021-05-22 18:49:33 +0700

    Update code

---
+++ app/controllers/Categories.php
@@ -0,0 +1,13 @@
+<?php 
+class Categories extends Controller{
+    
+    public $data = [], $model = [];
+    
+    public function __construct(){
+        //construct
+    }
+    
+    public function index(){
+        //Index
+    }
+}
